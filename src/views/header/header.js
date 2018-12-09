0cb091599f1dac32b8ab37347cac683b2a4d9e7d
HUGY1 <hugangyu@live.cn>
2018-12-10 00:01:31 +0800

    antd引入

---
+++ src/views/header/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './header.css';
+import { Layout } from 'antd'
+
+class Header1 extends Component{
+    render() {
+        return (
+            <Layout>
+                <Layout.Header></Layout.Header>
+            </Layout>
+        )
+    }
+   
+}
+
+export default Header1
