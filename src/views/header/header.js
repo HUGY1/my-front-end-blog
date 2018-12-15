e75194d538ca9c08173ddf04f921188e8373de37
HUGY1 <hugangyu@live.cn>
2018-12-15 23:55:40 +0800

    edit router

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './header.css';
 import { Layout, Menu, Icon } from 'antd'
-
+import {
+    NavLink,
+} from "react-router-dom";
 
 
 class Header extends Component {
@@ -9,7 +11,7 @@ class Header extends Component {
         current: 'mail',
     }
 
-    
+
     handleClick = (e) => {
         this.setState({
             current: e.key,
@@ -17,18 +19,18 @@ class Header extends Component {
     }
     render() {
         return (
-
             <Layout.Header className='g-header'>
                 <h1 className='m-title'>Welcome to my Blog</h1>
                 <Menu className='m-head'
                     onClick={this.handleClick}
                     selectedKeys={[this.state.current]}
                     mode="horizontal">
-                    <Menu.Item className='u-item' key="mail"><Icon type="solution" />我的文章</Menu.Item>
-                    <Menu.Item className='u-item' key="app"><Icon type="appstore" />心情</Menu.Item>
-                    <Menu.Item className='u-item' key="alipay"><Icon type="user" />关于我</Menu.Item>
+                    <Menu.Item className='u-item' key="mail"><NavLink to="/article/detail"><Icon type="solution" />我的文章</NavLink></Menu.Item>
+                    <Menu.Item className='u-item' key="app"><NavLink to="/article/list"><Icon type="appstore" />心情</NavLink></Menu.Item>
+                    <Menu.Item className='u-item' key="alipay"><NavLink to="/profile"><Icon type="user" />关于我</NavLink></Menu.Item>
                 </Menu>
             </Layout.Header>
+
 
         )
     }
--- src/views/layout/layout.js
+++ src/views/layout/layout.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd'
 import Header from '../header/header'
-
-class layout extends Component{
+import { Route , HashRouter, BrowserRouter} from "react-router-dom";
+// 路由页面
+import Detail from '../article/detail'
+import ArticleList from '../article/list'
+class layout extends Component {
     render() {
         return (
-            <Layout>
-                <Header></Header>
+            <BrowserRouter>
+                <Layout>
+                    <Header></Header>
+                    <Layout>
+                        <Route path="/article/detail" component={Detail} />
+                        <Route path="/article/list" component={ArticleList} />
+                    </Layout>
 
-                
-            </Layout>
+                </Layout>
+            </BrowserRouter>
+
         )
     }
-   
+
 }
 
 export default layout
