14209669dbbf27caef611fa846d8dee29151de3c
HUGY1 <hugangyu@live.cn>
2018-12-18 00:11:31 +0800

    try deploy

--- src/views/article/list.js
+++ src/views/article/list.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
-import { Row, Col, Divider } from 'antd'
+import { Row, Col } from 'antd'
 import './list.css'
 
 class ArticleList extends Component {
 
     render() {
-
+        
         return (
             <div>
-                <Row className='m-item' gutter={0}>
-                    <Col span={18} className='m-title'>
-                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+ <Row className='m-item' gutter={0}>
+                <Col span={18} className='m-title'>
+                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
                 </Col>
-                    <Col span={4} className='m-date'>
-                        2014.12.06
+                <Col span={4} className='m-date'>
+                    2014.12.06
                 </Col>
-                </Row>
-                <Row className='m-item' gutter={0}>
-                    <Col span={18} className='m-title'>
-                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+            </Row>
+            <Row className='m-item' gutter={0}>
+                <Col span={18} className='m-title'>
+                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
                 </Col>
-                    <Col span={4} className='m-date'>
-                        2014.12.06
+                <Col span={4} className='m-date'>
+                    2014.12.06
                 </Col>
-                </Row>
-                <Row className='m-item' gutter={0}>
-                    <Col span={18} className='m-title'>
-                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+            </Row>
+            <Row className='m-item' gutter={0}>
+                <Col span={18} className='m-title'>
+                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
                 </Col>
-                    <Col span={4} className='m-date'>
-                        2014.12.06
+                <Col span={4} className='m-date'>
+                    2014.12.06
                 </Col>
-                </Row>
-                <Row className='m-item' gutter={0}>
-                    <Col span={18} className='m-title'>
-                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+            </Row>
+            <Row className='m-item' gutter={0}>
+                <Col span={18} className='m-title'>
+                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
                 </Col>
-                    <Col span={4} className='m-date'>
-                        2014.12.06
+                <Col span={4} className='m-date'>
+                    2014.12.06
                 </Col>
-                </Row>
+            </Row>
             </div>
+           
 
-
-
+            
         )
     }
 
