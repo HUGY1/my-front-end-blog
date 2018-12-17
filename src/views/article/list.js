de874c92d45da77059e1319ead3b0af042722882
HUGY1 <hugangyu@live.cn>
2018-12-18 00:04:31 +0800

    edit list page

--- src/views/article/list.js
+++ src/views/article/list.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
-
+import { Row, Col, Divider } from 'antd'
+import './list.css'
 
 class ArticleList extends Component {
+
     render() {
+
         return (
-            <div>文章列表页面</div>
+            <div>
+                <Row className='m-item' gutter={0}>
+                    <Col span={18} className='m-title'>
+                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+                </Col>
+                    <Col span={4} className='m-date'>
+                        2014.12.06
+                </Col>
+                </Row>
+                <Row className='m-item' gutter={0}>
+                    <Col span={18} className='m-title'>
+                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+                </Col>
+                    <Col span={4} className='m-date'>
+                        2014.12.06
+                </Col>
+                </Row>
+                <Row className='m-item' gutter={0}>
+                    <Col span={18} className='m-title'>
+                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+                </Col>
+                    <Col span={4} className='m-date'>
+                        2014.12.06
+                </Col>
+                </Row>
+                <Row className='m-item' gutter={0}>
+                    <Col span={18} className='m-title'>
+                        文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+                </Col>
+                    <Col span={4} className='m-date'>
+                        2014.12.06
+                </Col>
+                </Row>
+            </div>
+
+
+
         )
     }
 
--- src/views/layout/layout.js
+++ src/views/layout/layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Layout } from 'antd'
 import Header from '../header/header'
 import { Route , BrowserRouter} from "react-router-dom";
+import './layout.css'
 // 路由页面
 import Detail from '../article/detail'
 import ArticleList from '../article/list'
@@ -11,7 +12,7 @@ class layout extends Component {
             <BrowserRouter>
                 <Layout>
                     <Header></Header>
-                    <Layout>
+                    <Layout className='m-main'>
                         <Route path="/article/detail" component={Detail} />
                         <Route path="/article/list" component={ArticleList} />
                     </Layout>
