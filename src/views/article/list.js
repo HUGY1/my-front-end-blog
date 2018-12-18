4d7de2b89f4d57429ea1f48d1720e9ea64455fb4
HUGY1 <hugangyu@live.cn>
2018-12-18 23:25:43 +0800

    调取github接口读取issue列表

--- src/views/article/list.js
+++ src/views/article/list.js
@@ -1,49 +1,47 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd'
 import './list.css'
-
+import axios from 'axios'
 class ArticleList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            articleList: []
+        }
+    }
 
+    componentDidMount() {
+        // 452d304c0aab88403202651a261552e168bccb7b
+        //  https://api.github.com https://api.github.com/issues
+        axios.get('https://api.github.com/repos/HUGY1/my-front-end-blog/issues?access_token=452d304c0aab88403202651a261552e168bccb7b').then(res => {
+            this.setState({
+                articleList: res.data
+            })
+        })
+    }
+
+   
     render() {
-        
+        function List(props) {
+            console.log(props)
+            let list = props.group.map(item => 
+                <Row className='m-item' gutter={0} key={item.id}>
+                    <Col span={16} className='m-title'>
+                        {item.title}
+                    </Col>
+                    <Col span={8} className='m-date'>
+                        {item.created_at}
+                    </Col>
+                </Row>
+            )
+            return (
+                <div>
+                    {list}
+                </div>
+            )
+        }
         return (
-            <div>
- <Row className='m-item' gutter={0}>
-                <Col span={18} className='m-title'>
-                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
-                </Col>
-                <Col span={4} className='m-date'>
-                    2014.12.06
-                </Col>
-            </Row>
-            <Row className='m-item' gutter={0}>
-                <Col span={18} className='m-title'>
-                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
-                </Col>
-                <Col span={4} className='m-date'>
-                    2014.12.06
-                </Col>
-            </Row>
-            <Row className='m-item' gutter={0}>
-                <Col span={18} className='m-title'>
-                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
-                </Col>
-                <Col span={4} className='m-date'>
-                    2014.12.06
-                </Col>
-            </Row>
-            <Row className='m-item' gutter={0}>
-                <Col span={18} className='m-title'>
-                    文章标题啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊
-                </Col>
-                <Col span={4} className='m-date'>
-                    2014.12.06
-                </Col>
-            </Row>
-            </div>
-           
-
-            
+            <List group={this.state.articleList} />
         )
     }
 
